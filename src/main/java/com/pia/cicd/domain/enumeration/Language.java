ce925c18bbda265750c7d7ea2d77720c6a4e027d
irfannisanci <irfan.nisanci@pia-team.com>
2022-05-24 15:22:19 +0300

    first commit

---
+++ src/main/java/com/pia/cicd/domain/enumeration/Language.java
@@ -0,0 +1,10 @@
+package com.pia.cicd.domain.enumeration;
+
+/**
+ * The Language enumeration.
+ */
+public enum Language {
+    FRENCH,
+    ENGLISH,
+    SPANISH,
+}
---
+++ src/main/java/com/pia/cicd/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.pia.cicd.repository;
+
+import com.pia.cicd.domain.Department;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data SQL repository for the Department entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {}
